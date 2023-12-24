203f372dd26a9c2eb31d9821b369ae1763479323
Kirill Palianitsa <kirillpal22@gmail.com>
2023-12-24 15:12:34 +0300

    Task 4

---
+++ Task 4/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double n = scanner.nextDouble();
+        double a = Math.round(n);
+        System.out.println(a);
+    }
+}
