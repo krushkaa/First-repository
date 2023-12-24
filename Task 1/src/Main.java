f4da1e1fb7be0db81a33f996f0c61c68b4681a37
Kirill Palianitsa <kirillpal22@gmail.com>
2023-12-24 14:25:39 +0300

    Task 1

---
+++ Task 1/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a;
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        System.out.println(a=4*(b+c-1)/2);
+    }
+}
