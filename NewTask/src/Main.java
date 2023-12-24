396016327536d44d3347dba6d56e2810cc47a831
Kirill Palianitsa <kirillpal22@gmail.com>
2023-12-24 14:07:40 +0300

    Hello <user>

--- NewTask/src/Main.java
+++ NewTask/src/Main.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Scanner scanner = new Scanner(System.in);
+        String usersWord = scanner.nextLine();
+        System.out.println("Hello " + usersWord);
     }
 }
